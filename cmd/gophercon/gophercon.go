afbbc5471df2c6a56d7897c76dbd3aa3858d8637
Freyr Magnusson <freyr.magnusson@gmail.com>
2018-05-31 10:59:35 +0000

    routing package added

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/freyzor/gophercon/pkg/routing"
 )
 
 func main() {
 	log.Printf("Service is staring..")
-	r := mux.NewRouter()
-	r.HandleFunc("/home", homeHandler()).Methods(http.MethodGet)
+	r := routing.BaseRouter()
 	http.ListenAndServe(":8000", r)
 	log.Printf("Service is stopping..")
-}
-
-func homeHandler() func(http.ResponseWriter, *http.Request){
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Request is processing: %s", r.URL.Path)
-		fmt.Fprint(w, "Hello! ")
-	}
 }
